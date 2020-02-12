b6ca77117c0c352f9ea32f51c0b4d1de3a5011bd
Manoj Samanta <samanta@homolog.us>
2020-02-12 23:10:14 +0000

    fixed  bug, now this program correctly summarizes results

--- lib/winner_web/views/winner_view.ex
+++ lib/winner_web/views/winner_view.ex
@@ -1,3 +1,10 @@
 defmodule WinnerWeb.WinnerView do
   use WinnerWeb, :view
+
+  def number_joined(resource, map) do
+   case map[resource] do
+    nil -> 0
+    _   -> Enum.count(map[resource])
+   end
+  end
 end
